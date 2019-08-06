f496aa8578d33ea693aed8e01d569ec332eeb185
aspenjames <aspenjames@tqca.org>
2019-08-05 20:07:16 -0700

    passing append/prepend spec

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,4 +5,35 @@ class LinkedList
 		@head = nil
 		@tail = nil
 	end
+
+	def append(data = nil)
+		return false if !data
+		el = Element.new(data)
+		if !@head
+			@head = el
+			@tail = el
+		elsif @head == @tail
+			@head.next = el
+			@tail = el
+		else
+			@tail.next = el
+			@tail = el
+		end
+
+		true
+	end
+
+	def prepend(data = nil)
+		return false if !data
+		el = Element.new(data)
+		if !@head
+			@head = el
+			@tail = el
+		else
+			el.next = @head
+			@head = el
+		end
+
+		true
+	end
 end
